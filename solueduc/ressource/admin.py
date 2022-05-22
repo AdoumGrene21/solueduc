da823905c6de12149a7d58f4faa28d0bfd338bb8
Oumar3 <oumaralitori1@gmail.com>
2022-05-22 09:01:57 +0100

    migration de base de donnée

--- solueduc/ressource/admin.py
+++ solueduc/ressource/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Etudiant,Cours,Salle,Enseignant
 # Register your models here.
+
+admin.site.register(Etudiant)
+admin.site.register(Salle)
+admin.site.register(Cours)
+admin.site.register(Enseignant)
+
--- solueduc/ressource/models.py
+++ solueduc/ressource/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+# Create your mode
+
+class Salle(models.Model):
+    libelle = models.CharField('Nom salle', max_length=120)
+    description = models.CharField('Nom Cours', max_length=120)
+
+    def __str__(self):
+        return self.libelle
+
+class Cours(models.Model):
+    libelle = models.CharField('Nom Cours', max_length=120)
+    description = models.CharField('Nom Cours', max_length=120)
+    def __str__(self):
+        return self.libelle
+
+class Enseignant(models.Model):
+    nom = models.CharField('nom', max_length=120)
+    prenoms = models.CharField('prenom', max_length=20)
+    date_naissance = models.DateField()
+    sexe = models.CharField('sexe', max_length=120)
+    specialite = models.CharField('specialite', max_length=120)
+    cours = models.ManyToManyField(Cours, blank=False)
+
+        
+    def __str__(self):
+        return self.nom
+
+class Etudiant(models.Model):
+    nom = models.CharField('Nom', max_length=120)
+    prenoms = models.CharField('Prenoms', max_length=200)
+    date_naissance = models.DateField()
+    sexe = models.CharField('Sexe', max_length=120)
+    address= models.CharField(max_length=220)
+    nationalite = models.CharField(max_length=120)
+    salle = models.ForeignKey(Salle, blank=False, null=False, on_delete=models.CASCADE)
+    cours = models.ManyToManyField(Cours, blank=False)
+
+
+    def __str__(self):
+        return self.nom
+
+
+
+   # description = models.TextField(blank=True)
+    #teachers = models.ManyToManyField(Teacher, blank=False)
+    #courses = models.ManyToManyField(Coures, blank=False)
+        
